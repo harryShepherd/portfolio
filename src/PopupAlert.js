2f9648677bcdd7bb753f30efbe7bfddd2095e266
harryShepherd <harryshepherd0.0@hotmail.com>
2025-08-07 21:47:08 +0100

    Added WIP modal

--- src/PopupAlert.js
+++ src/PopupAlert.js
@@ -1,5 +1,6 @@
-import { React, useState } from "react";
-import { Alert, TitleBar } from "@react95/core";
+import { React, useState } from "react"
+import { List, Modal, TitleBar } from "@react95/core"
+import { Computer } from "@react95/icons";
 
 function Popup() {
     const [isVisible, setVisible] = useState(true);
@@ -9,18 +10,22 @@ function Popup() {
     };
 
     return (
-        isVisible && <Alert 
-        flexDirection='row-reverse'
-        title='WIP'
-        type='error'
-        message='This website is currently work in progress.'
-        titleBarOptions={<TitleBar.Close key='close' onClick={handleHide}/>}
-        buttons={[{
-            value: 'OK',
-            onClick: handleHide
-        }]}
-        />
-    );
+        isVisible && <Modal 
+        icon={<Computer variant='16x16_4' />}
+        title='Work in progress'
+        dragOptions={{defaultPosition: {x: 500,y: 100}}}
+        titleBarOptions={<TitleBar.Close key='close' onClick={handleHide} />}
+        menu={[{
+            name: 'File',
+            list: <List width="200px">
+                        <List.Item onClick={handleHide}>Exit</List.Item>
+                      </List>
+          }]}>
+        <Modal.Content width="300px" height="100px" boxShadow="$in" bgColor="white">
+            This website is work in progress.
+        </Modal.Content>
+        </Modal>
+    )
 }
 
 export default Popup;
